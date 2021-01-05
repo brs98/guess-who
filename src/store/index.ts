846d8659bda9de1090420a581e0b9e1911ef25e8
Brandon Southwick <southwick.brandon21@gmail.com>
2021-01-05 12:41:20 -0700

    Add logout method

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,50 +1,40 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import createPersistedState from "vuex-persistedstate";
 import axios from 'axios';
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    token: sessionStorage.getItem('user-token') || '',
+    token: sessionStorage.getItem('userToken') || '',
     status: '',
   },
-  plugins: [createPersistedState()],
   mutations: {
     login: () => {
-      sessionStorage.setItem('user-token', 'good');
       axios.get('https://auth.fhtl.byu.edu'+'?redirect=' + window.location.origin + '/&site=key').then((res) => {
         window.location = res.request.responseURL;
       }).catch((res) => {
         console.log(res);
       });
+      sessionStorage.setItem('userToken', 'good');
     },
-    logout: () => {
-      sessionStorage.removeItem('user-token');
-      /**
-      $.ajax({
-         url:"https://api.familysearch.org/platform/logout",
-         headers: {
-             "Authorization":"Bearer " + this.getAccessToken()
-         },
-         type:"POST",
-         success: function(result){
-               console.log(result);
-               sessionStorage.setItem("token", "bad");
-               this.state = {};
-         },
-         error:function(error){
-               console.log(error)}
-         })
-         */
+    logout(state) {
+      state.status = ''
+      state.token = ''
     }
   },
   actions: {
     login: context => context.commit('login'),
-    logout: context => context.commit('logout')
+    logout({commit}){
+      return new Promise((resolve, reject) => {
+        commit('logout')
+        sessionStorage.removeItem('userToken')
+        delete axios.defaults.headers.common['Authorization']
+        resolve()
+      })
+    }
   },
   getters : {
-    isLoggedIn: state => !!state.token,
+    isLoggedIn: state => !!state.token
   }
 })
