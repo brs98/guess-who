fde270be8f0fd7a83f01255e56f18cca07c6e9f1
Brandon Southwick <southwick.brandon21@gmail.com>
2021-01-05 11:57:59 -0700

    Add login method

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,26 +1,50 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 import createPersistedState from "vuex-persistedstate";
-
+import axios from 'axios';
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    token: '', //change to "token: getCookie..."
-    isLoggedIn: false //delete this and change the getter for isLoggedIn after adding token functionality
+    token: sessionStorage.getItem('user-token') || '',
+    status: '',
   },
   plugins: [createPersistedState()],
   mutations: {
-    login: state => state.isLoggedIn = true, //modify to actual login process
-    logout: state => state.isLoggedIn = false //modify to actual logout process
-
+    login: () => {
+      sessionStorage.setItem('user-token', 'good');
+      axios.get('https://auth.fhtl.byu.edu'+'?redirect=' + window.location.origin + '/&site=key').then((res) => {
+        window.location = res.request.responseURL;
+      }).catch((res) => {
+        console.log(res);
+      });
+    },
+    logout: () => {
+      sessionStorage.removeItem('user-token');
+      /**
+      $.ajax({
+         url:"https://api.familysearch.org/platform/logout",
+         headers: {
+             "Authorization":"Bearer " + this.getAccessToken()
+         },
+         type:"POST",
+         success: function(result){
+               console.log(result);
+               sessionStorage.setItem("token", "bad");
+               this.state = {};
+         },
+         error:function(error){
+               console.log(error)}
+         })
+         */
+    }
   },
   actions: {
     login: context => context.commit('login'),
     logout: context => context.commit('logout')
   },
   getters : {
-    isLoggedIn: state => !!state.isLoggedIn, //change !!state.isLoggedIn to !!state.token after adding token functionality
+    isLoggedIn: state => !!state.token,
   }
 })
